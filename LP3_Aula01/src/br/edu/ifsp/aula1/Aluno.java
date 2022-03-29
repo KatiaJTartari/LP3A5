89490e6da1d8d7eaad3b3c82ffdad887c4002fd4
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-03-29 18:05:02 -0300

    Update Aluno.java

--- LP3_Aula01/src/br/edu/ifsp/aula1/Aluno.java
+++ LP3_Aula01/src/br/edu/ifsp/aula1/Aluno.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.aula1;
 
 public class Aluno {
 	
-	private String matricula;
+    private String matricula;
     private String nome;
     final private Disciplina disciplina;
            
@@ -10,19 +10,19 @@ public class Aluno {
 		// TODO Auto-generated method stub
 	    Aluno aluno1 = new Aluno("SP3013251", "Katia J Tartari"); 
 	    
-	    aluno1.disciplina.setNomeDisciplina("LP3A5 - Linguagem de Programação III");
+	    aluno1.disciplina.setNomeDisciplina("LP3A5 - Linguagem de ProgramaÃ§Ã£o III");
 	    aluno1.disciplina.setQtdPresenca(18);
 	    aluno1.disciplina.setNotaExercicios(10f);
 	    aluno1.disciplina.setNotaProva(7f);
 	    aluno1.disciplina.setNotaTrabalho(8f);
 	    	    
-	    System.out.println("Aluno: " + aluno1.getNome() + " - Matrícula: " + aluno1.getMatricula());
+	    System.out.println("Aluno: " + aluno1.getNome() + " - MatrÃ­cula: " + aluno1.getMatricula());
 	    System.out.println("Disciplina: " + aluno1.disciplina.getNomeDisciplina());
-	    System.out.println("Quantidade Total de Presenças: " + aluno1.disciplina.getQtdPresenca());
-	    System.out.println("Nota Exercícios: " + aluno1.disciplina.getNotaExercicios() + 
+	    System.out.println("Quantidade Total de PresenÃ§as: " + aluno1.disciplina.getQtdPresenca());
+	    System.out.println("Nota ExercÃ­cios: " + aluno1.disciplina.getNotaExercicios() + 
 	    		           " - Nota Prova: " + aluno1.disciplina.getNotaProva() + 
 	    		           " - Nota Trabalho: " + aluno1.disciplina.getNotaTrabalho());
-	    System.out.println("Média Final: " + aluno1.disciplina.calculaMediaFinal());
+	    System.out.println("MÃ©dia Final: " + aluno1.disciplina.calculaMediaFinal());
 	    aluno1.disciplina.passouDeAno();
 	}
     
