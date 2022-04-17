ba6982da82b84a9923b65b706a99987fd070de6d
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-04-17 19:54:27 -0300

    Add files via upload

---
+++ LP3_Aula02/src/br/edu/ifsp/aula2/HelloWorldRunnable1.java
@@ -0,0 +1,20 @@
+package br.edu.ifsp.aula2;
+
+public class HelloWorldRunnable1 implements Runnable {
+
+	@Override
+
+	public void run() {
+		System.out.println("Aqui quem fala é a " + Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Estamos na: " + Thread.currentThread().getName());
+
+		Runnable runnable = new HelloWorldRunnable1();
+
+		runnable.run();
+	}
+
+}
---
+++ LP3_Aula02/src/br/edu/ifsp/aula2/ThreadPrincipal.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.aula2;
+
+public class ThreadPrincipal {
+
+	public static void main(String[] args) {
+
+		Thread principal = Thread.currentThread();
+		System.out.println("Nome da thread: " + principal.getName());
+		System.out.println("Thread.toString(): " + principal);
+	}
+
+}
