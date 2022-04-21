7b33971bae410193af4aef56f848c267b8535783
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-04-21 20:30:42 -0300

    Add files via upload

---
+++ LP3_Aula03/src/br/edu/ifsp/lp3a5/jantarFilosofos/IRoteiroJantar.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.lp3a5.jantarFilosofos;
+
+public interface IRoteiroJantar {
+
+	public void jantarPensante(Filosofo filosofo) throws InterruptedException;
+
+}
---
+++ LP3_Aula03/src/br/edu/ifsp/lp3a5/jantarFilosofos/solucao3/StatusFilosofo.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.lp3a5.jantarFilosofos.solucao3;
+
+public enum StatusFilosofo {
+	PENSANDO, COMENDO, FOME;
+}
