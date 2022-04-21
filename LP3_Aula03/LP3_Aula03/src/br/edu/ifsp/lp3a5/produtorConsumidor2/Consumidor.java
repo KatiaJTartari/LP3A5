6d6abe3eb8bd37c6ccbe166c365e06d86c3e7301
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-04-21 20:23:05 -0300

    Add files via upload

---
+++ LP3_Aula03/LP3_Aula03/src/br/edu/ifsp/lp3a5/produtorConsumidor2/Consumidor.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.lp3a5.produtorConsumidor2;
+
+// Classe Consumidor: o objeto encadeado que está consumindo entradas da fila
+public class Consumidor implements Runnable {
+	Fila f;
+
+	Consumidor(Fila f) {
+		this.f = f;
+		new Thread(this, "Consumidor").start();
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++)
+			// Consumidor obtém/remove itens
+			f.get();
+	}
+}
