93dd95264ab9fd9c385b849747bcaa0f976cb725
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-05-05 17:47:31 -0300

    Update CriandoStreams.java

--- LP3_Aula06/src/br/edu/ifsp/lp3a5/exemplosStreams/CriandoStreams.java
+++ LP3_Aula06/src/br/edu/ifsp/lp3a5/exemplosStreams/CriandoStreams.java
@@ -13,38 +13,37 @@ public class CriandoStreams {
 
 	private static void streams() {
 
-		// A interface collection possui o método stream e parallelStream
+		// A interface collection possui o mÃ©todo stream e parallelStream
 		final List<String> lista = Arrays.asList("amor", "bola", "casa", "dado");
 
 		System.out.println("Criando stream a partir de uma lista");
-		// Criação de stream a partir de uma lista
+		// CriaÃ§Ã£o de stream a partir de uma lista
 		lista.stream().forEach(System.out::println);
 
 		System.out.println("\nExemplo com parallelStream");
 		/*
-		 * O parallelStream é para programação paralela, situações em que não é
-		 * necessário que o fluxo de processamento seja linear
+		 * O parallelStream Ã© para programaÃ§Ã£o paralela, situaÃ§Ãµes em que nÃ£o Ã©
+		 * necessÃ¡rio que o fluxo de processamento seja linear
 		 */
 		lista.parallelStream().forEach(System.out::println);
 
-		// Também é possível criar streams a partir de arrays
+		// TambÃ©m Ã© possÃ­vel criar streams a partir de arrays
 		String[] nomes = new String[] { "Ana", "Beatriz", "Clara", "Diana", "Eliane" };
 		System.out.println("\nStream a partir de array usando a classe java.util.Arrays");
 		Arrays.stream(nomes).forEach(System.out::println);
 
-		//É possível criar um Stream também com a própria classe Stream usando o método of
-		 
-		System.out.println("\nAo usar o método Stream.of podem ser passados Object");
+		//Ã‰ possÃ­vel criar um Stream tambÃ©m com a prÃ³pria classe Stream usando o mÃ©todo of
+		System.out.println("\nAo usar o mÃ©todo Stream.of podem ser passados Object");
 		Stream.of("casa", 1, "teste").forEach(System.out::println);
 
-		// Para números é possível gerar um intervalo
-		System.out.println("\nCriando um stream com o método IntStream.range");
+		// Para nÃºmeros Ã© possÃ­vel gerar um intervalo
+		System.out.println("\nCriando um stream com o mÃ©todo IntStream.range");
 		IntStream.range(10, 20).forEach(System.out::println);
-		// caso deseje incluir o último número pode usar o rangeClosed
+		// caso deseje incluir o Ãºltimo nÃºmero pode usar o rangeClosed
 
-		// Outra forma de criar um stream é o método iterate.
-		// Inicia-se com uma semente e uma função que define como ela será transformada
-		System.out.println("\nCriando um stream com o método Stream.iterate");
+		// Outra forma de criar um stream Ã© o mÃ©todo iterate.
+		// Inicia-se com uma semente e uma funÃ§Ã£o que define como ela serÃ¡ transformada
+		System.out.println("\nCriando um stream com o mÃ©todo Stream.iterate");
 		Stream.iterate(true, valor -> !valor).limit(5).forEach(System.out::println);
 
 	}
