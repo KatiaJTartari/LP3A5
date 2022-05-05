9058037299c9e4f04de69f4cdefcff881ec019b3
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-05-05 17:28:56 -0300

    Update ExemploSupplierConsumer.java

--- LP3_Aula06/src/br/edu/ifsp/lp3a5/exemplosInterfacesFuncionais/ExemploSupplierConsumer.java
+++ LP3_Aula06/src/br/edu/ifsp/lp3a5/exemplosInterfacesFuncionais/ExemploSupplierConsumer.java
@@ -10,18 +10,18 @@ public class ExemploSupplierConsumer {
 	}
 
 	/**
-	 * Supplier: fornecedor - n„o recebe nada e retorna um valor Consumer :
-	 * consumidor - recebe um valor e n„o retorna nada
+	 * Supplier: fornecedor - n√£o recebe nada e retorna um valor 
+	 * Consumer: consumidor - recebe um valor e n√£o retorna nada
 	 */
 	private static void exemploSupplierConsumer() {
-		Stream.generate(() -> new Random().nextInt()) // A funÁ„o generate recebe um supplier
-				// Supplier: interface funcional que contÈm um mÈtodo get sem argumentos
-				// Ou seja, n„o recebe input, mas devolve um output
+		Stream.generate(() -> new Random().nextInt()) // A fun√ß√£o generate recebe um supplier
+				// Supplier: interface funcional que cont√©m um m√©todo get sem argumentos
+				// Ou seja, n√£o recebe input, mas devolve um output
 				.limit(10) // limita a 10 valores
-				.forEach((e) -> System.out.println(e)); // a funÁ„o forEach recebe um consumer
-		// Consumer: interface funcional que contÈm um mÈtodo accept com um par‚metro
-		// de entrada e void na saÌda
-		// Consumer: oposto do supplier, n„o retorna nada, mas recebe um valor com o
-		// qual faz alguma aÁ„o.
+				.forEach((e) -> System.out.println(e)); // a fun√ß√£o forEach recebe um consumer
+		// Consumer: interface funcional que cont√©m um m√©todo accept com um par√¢metro
+		// de entrada e void na sa√≠da
+		// Consumer: oposto do supplier, n√£o retorna nada, mas recebe um valor com o
+		// qual faz alguma a√ß√£o.
 	}
 }
