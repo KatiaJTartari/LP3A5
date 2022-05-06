d8ff1834faf411e89aff816381da8dc84515c5e6
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-05-06 12:48:44 -0300

    Update Exercicio_Exemplo3.java

--- LP3_Aula06/src/br/edu/ifsp/lp3a5/exerciciosStreamsAPI/Exercicio_Exemplo3.java
+++ LP3_Aula06/src/br/edu/ifsp/lp3a5/exerciciosStreamsAPI/Exercicio_Exemplo3.java
@@ -8,7 +8,7 @@ public class Exercicio_Exemplo3 {
 
 	public static void main(String[] args) {
 
-		System.out.println("\tMétodo Stream.flatMap() e Collectors.toList()");
+		System.out.println("\tMétodos .flatMap() e Collectors.toList()");
 
 		/*
 		 * Função Stream.flatMap() obtém um único List contendo todos os elementos de
