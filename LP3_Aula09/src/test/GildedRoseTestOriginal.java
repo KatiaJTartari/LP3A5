5488f98d9710ae2b137cb55b98da1ca7fb780e64
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-06-16 13:43:56 -0300

    Add files via upload

---
+++ LP3_Aula09/src/test/GildedRoseTestOriginal.java
@@ -0,0 +1,22 @@
+package test;
+
+// Classe GildedRoseTestOriginal original sem modificação.
+
+import org.junit.jupiter.api.Test;
+
+import main.GildedRose;
+import main.Item;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTestOriginal {
+
+	@Test
+	void foo() {
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("fixme", app.items[0].name);
+	}
+
+}
